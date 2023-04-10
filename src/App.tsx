dea9204a0e5955b16d6d00fb90ea4cee3f51f9ab
Harrison Travers <harrisontravers@Harrisons-MacBook-Pro.local>
2023-04-10 22:22:51 +1000

    Very basic back-end integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MESSAGE = gql`
+  query GetUser($id: Int!) {
+    user(id: $id) {
+      id
+      email
+    }
+  }
+`;
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [count, setCount] = useState(0);
+
+  const { loading, error, data } = useQuery(GET_MESSAGE, {
+    variables: { id: count },
+  });
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={() => setCount(count + 1)}>
+          Hallo min ven. You click dis one slowly now.
+        </button>
+        <div>{data?.user?.email || ""}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const client = new ApolloClient({
+  uri: "https://test-back-end.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
